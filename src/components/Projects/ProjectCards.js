1181cc7f5656ec9bc893b5550d7649a41bdf4ba5
Nishadi Priyangika Samarathunge Malwalage <104554991+nishadipri@users.noreply.github.com>
2026-02-20 07:06:41 +0100

    Fix alt attribute for Card.Img and improve button rendering logic in ProjectCards component

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -7,26 +7,25 @@ import { BsGithub } from "react-icons/bs";
 function ProjectCards(props) {
   return (
     <Card className="project-card-view">
-      <Card.Img variant="top" src={props.imgPath} alt="card-img" />
+      <Card.Img variant="top" src={props.imgPath} alt={props.title} />
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
         <Card.Text style={{ textAlign: "justify" }}>
           {props.description}
         </Card.Text>
-        <Button variant="primary" href={props.ghLink} target="_blank">
-          <BsGithub /> &nbsp;
-          {props.isBlog ? "Blog" : "GitHub"}
-        </Button>
-        {"\n"}
-        {"\n"}
+        {props.ghLink && props.ghLink !== "#" && (
+          <Button variant="primary" href={props.ghLink} target="_blank" rel="noopener noreferrer">
+            <BsGithub /> &nbsp;
+            {props.isBlog ? "Blog" : "GitHub"}
+          </Button>
+        )}
 
-        {/* If the component contains Demo link and if it's not a Blog then, it will render the below component  */}
-
-        {!props.isBlog && props.demoLink && (
+        {!props.isBlog && props.demoLink && props.demoLink !== "#" && (
           <Button
             variant="primary"
             href={props.demoLink}
             target="_blank"
+            rel="noopener noreferrer"
             style={{ marginLeft: "10px" }}
           >
             <CgWebsite /> &nbsp;
