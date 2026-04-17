db5aacaf8b01385391a99f397688c936c3383d00
Nishadi Priyangika Samarathunge Malwalage <104554991+nishadipri@users.noreply.github.com>
2026-04-17 16:14:49 +0200

    Update alt text for About section image to improve accessibility

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -31,7 +31,7 @@ function About() {
             style={{ paddingTop: "120px", paddingBottom: "50px" }}
             className="about-img"
           >
-            <img src={laptopImg} alt="about" className="img-fluid" loading="lazy" width="500" height="500" />
+            <img src={laptopImg} alt="Developer working on a laptop" className="img-fluid" loading="lazy" width="500" height="500" />
           </Col>
         </Row>
         <h1 className="project-heading">
