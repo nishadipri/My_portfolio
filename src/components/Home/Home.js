e84a826c2f106a5295163194a0264f4305621a8d
Nishadi Priyangika Samarathunge Malwalage <104554991+nishadipri@users.noreply.github.com>
2026-05-26 16:49:42 +0200

    remove wave emoji from Home component heading for cleaner presentation

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,9 +22,6 @@ function Home() {
             <Col md={7} className="home-header">
               <h1 style={{ paddingBottom: 15 }} className="heading">
                 Hi There!{" "}
-                <span className="wave" role="img" aria-labelledby="wave">
-                  👋🏻
-                </span>
               </h1>
 
               <h1 className="heading-name">
